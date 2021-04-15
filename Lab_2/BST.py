19c2fc6957dc44d374e809a60df737e7f4d6c5e1
Aleksandra Anna Sypuła <a.sypula@stud.elka.pw.edu.pl>
2021-04-15 18:39:47 +0000

    BST

---
+++ Lab_2/BST.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.number = 1
+        self.left = None
+        self.right = None
+
+    def increment(self):
+        self.number = self.number + 1
+
+    def decrement(self):
+        self.number = self.number - 1
+
+
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print(root.key, end=" ")
+        inorder(root.right)
+
+
+def insert(node, key):
+    if node is None:
+        return Node(key)
+    if key < node.key:
+        node.left = insert(node.left, key)
+    elif (key == node.key):
+        node.increment()
+    else:
+        node.right = insert(node.right, key)
+    return node
+
+
+def minValueNode(node):
+    current = node
+    while(current.left is not None):
+        current = current.left
+    return current
+
+
+def remove(root, key):
+    if root is None:
+        return root
+    if key < root.key:
+        root.left = remove(root.left, key)
+    elif(key > root.key):
+        root.right = remove(root.right, key)
+    else:
+        if root.number > 1:
+            root.decrement()
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = minValueNode(root.right)
+            root.key = temp.key
+            root.right = remove(root.right, temp.key)
+    return root
+
